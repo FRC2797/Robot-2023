9ee30def579de51c6ce5201a965a417264310a50
Joel Singh <joelsingh788@gmail.com>
2023-01-13 15:29:21 -0500

    Add teleopDrive command and assign it as the default command of drivetrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,13 +3,20 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Drivetrain;
+
+import static edu.wpi.first.wpilibj2.command.Commands.*;
+import static edu.wpi.first.math.MathUtil.applyDeadband;
 
 public class RobotContainer {
 
   private final CommandXboxController controller =
       new CommandXboxController(0);
 
+  private final Drivetrain drivetrain = new Drivetrain();
+
   public RobotContainer() {
+    drivetrain.setDefaultCommand(teleopDrive());
     configureBindings();
   }
 
@@ -18,5 +25,20 @@ public class RobotContainer {
 
   public Command getAutonomousCommand() {
     return null;
+  }
+
+  public Command teleopDrive() {
+    return run(() -> {
+      final double DEADBAND = 0.1;
+
+
+      double leftY = controller.getLeftY();
+      double rightX = controller.getRightX();
+
+      double leftYDeadband = applyDeadband(leftY, DEADBAND);
+      double rightXDeadband = applyDeadband(rightX, DEADBAND);
+
+      drivetrain.arcadeDrive(-leftYDeadband, rightXDeadband);
+    }, drivetrain);
   }
 }
