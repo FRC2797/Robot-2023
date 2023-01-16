5ea345c757749b20a1411104b21003337c6203e9
Joel Singh <joelsingh788@gmail.com>
2023-01-16 09:52:31 -0500

    Working driveUntilLevelOnChargingStation Command
    Working driveUntilLevelOnChargingStation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ import frc.robot.subsystems.Drivetrain;
 
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 import static edu.wpi.first.math.MathUtil.applyDeadband;
+import frc.robot.subsystems.Navx;
 
 public class RobotContainer {
 
@@ -14,6 +15,7 @@ public class RobotContainer {
       new CommandXboxController(0);
 
   private final Drivetrain drivetrain = new Drivetrain();
+  private final Navx navx = new Navx();
 
   public RobotContainer() {
     drivetrain.setDefaultCommand(teleopDrive());
@@ -21,6 +23,7 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+    controller.y().onTrue(driveUntilLevelOnChargingStation());
   }
 
   public Command getAutonomousCommand() {
@@ -40,5 +43,29 @@ public class RobotContainer {
 
       drivetrain.arcadeDrive(-leftYDeadband, rightXDeadband);
     }, drivetrain);
+  }
+
+  public Command driveUntilLevelOnChargingStation() {
+    final double SLOW_SPEED_FORWARD = 0.15;
+    final double SLOW_SPEED_BACKWARD = -0.05;
+    final double WAIT_BEFORE_OVERSHOOT_CORRECTION = 0.5;
+    final double PITCHED_VALUE = 15;
+
+
+    Command driveForwardSlowly = run(() -> drivetrain.arcadeDrive(SLOW_SPEED_FORWARD, 0), drivetrain);
+    Command waitUntilPitched = waitUntil(() -> Math.abs(navx.getRoll()) > PITCHED_VALUE);
+
+    Command driveBackwardSlowly = run(() -> drivetrain.arcadeDrive(SLOW_SPEED_BACKWARD, 0), drivetrain);
+
+    return (
+      deadline(waitUntilPitched.andThen(waitUntilLevel()), driveForwardSlowly)
+      .andThen(deadline(waitSeconds(WAIT_BEFORE_OVERSHOOT_CORRECTION).andThen(waitUntilLevel()), driveBackwardSlowly))
+    );
+  }
+
+  public Command waitUntilLevel() {
+    final double LEVELED_VALUE = 1;
+
+    return waitUntil(() -> Math.abs(navx.getRoll()) < LEVELED_VALUE);
   }
 }
