4dbb44912940d1f9c20943ab15ea0e7f9e2206c5
Joel Singh <joelsingh788@gmail.com>
2023-02-04 12:54:08 -0500

    Update OUTPUT constant

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -48,7 +48,7 @@ public class Drivetrain extends SubsystemBase {
     backRightEnc = backRight.getEncoder();
     backLeftEnc = backLeft.getEncoder();
 
-    final double OUTPUT_ROTATION_IN_INPUT_ROTATION = 1/8.19;
+    final double OUTPUT_ROTATION_IN_INPUT_ROTATION = 1/8.45864661654;
 
     frontRightEnc.setPositionConversionFactor(OUTPUT_ROTATION_IN_INPUT_ROTATION);
     frontLeftEnc.setPositionConversionFactor(OUTPUT_ROTATION_IN_INPUT_ROTATION);
