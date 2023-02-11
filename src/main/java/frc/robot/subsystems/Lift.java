b064bda37ad1c93fe063a4ea522c8b8ef8d55a09
Joel Singh <joelsingh788@gmail.com>
2023-02-11 10:13:22 -0500

    Add encoder to lift

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -9,12 +10,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Lift extends SubsystemBase {
   final private CANSparkMax motor;
+  final private RelativeEncoder encoder;
 
   public Lift() {
     final int MOTOR_ID = 3;
     final boolean IS_INVERTED = true;
     motor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
     motor.setInverted(IS_INVERTED);
+
+    encoder = motor.getEncoder();
 
     configureShuffleboard();
   }
@@ -26,5 +30,6 @@ public class Lift extends SubsystemBase {
   private void configureShuffleboard() {
     ShuffleboardTab lift = Shuffleboard.getTab("Lift");
     lift.addDouble("Motor speed", motor::get);
+    lift.addDouble("Encoder", encoder::getPosition);
   }
 }
