a6ac4d7054e5aefa7b196c0ba0ffc1a9b73720a4
Joel Singh <joelsingh788@gmail.com>
2023-02-18 12:26:34 -0500

    Working lift to position

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -14,11 +15,17 @@ public class Lift extends SubsystemBase {
 
   public Lift() {
     final int MOTOR_ID = 3;
-    final boolean IS_INVERTED = true;
+    final boolean IS_INVERTED = false;
+    final boolean IS_BRAKED = false;
+
     motor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
     motor.setInverted(IS_INVERTED);
+    motor.setIdleMode(IS_BRAKED ? IdleMode.kBrake : IdleMode.kCoast);
 
     encoder = motor.getEncoder();
+    final double ENCODER_COUNT_IN_A_SINGLE_ROTATION = 17.4;
+    encoder.setPositionConversionFactor(1 / ENCODER_COUNT_IN_A_SINGLE_ROTATION);
+    encoder.setPosition(0);
 
     configureShuffleboard();
   }
@@ -27,9 +34,21 @@ public class Lift extends SubsystemBase {
     motor.set(speed);
   }
 
+  public void setVoltage(double voltage) {
+    motor.setVoltage(voltage);
+  }
+
+  /**
+ * @return angle in radians from the horizontal
+ */
+public double getAngleInRadians() {
+    return encoder.getPosition() * 2 * Math.PI;
+}
+
   private void configureShuffleboard() {
     ShuffleboardTab lift = Shuffleboard.getTab("Lift");
     lift.addDouble("Motor speed", motor::get);
-    lift.addDouble("Encoder", encoder::getPosition);
+    lift.addDouble("Encoder Position", encoder::getPosition);
+    lift.addDouble("Get Angle in Radians", this::getAngleInRadians);
   }
 }
