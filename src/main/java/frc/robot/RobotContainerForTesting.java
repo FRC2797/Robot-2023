1d85cade62ec8dec2dd0dea450249863f2ff5a88
Joel Singh <joelsingh788@gmail.com>
2023-02-21 16:34:31 -0500

    In progress control loop tuning

--- src/main/java/frc/robot/RobotContainerForTesting.java
+++ src/main/java/frc/robot/RobotContainerForTesting.java
@@ -6,34 +6,38 @@ import static edu.wpi.first.wpilibj2.command.Commands.*;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class RobotContainerForTesting {
   Lift lift = new Lift();
   CommandJoystick joystick = new CommandJoystick(0);
   public RobotContainerForTesting() {
-    joystick.frontTrigger.onTrue(liftToPosition(Math.PI));
+    joystick.five.toggleOnTrue(liftToPosition(0.5 * 2 * Math.PI));
+    joystick.six.toggleOnTrue(liftToPosition(0.25 * 2 * Math.PI));
+    joystick.three.toggleOnTrue(liftToPosition(0.1 * 2 * Math.PI));
   }
 
   private CommandBase liftToPosition(double positionRadians) {
-    final double kS = 0.1593;
-    final double kV = 0.19465;
-    final double kA = 0.010118;
-    final double kG = 0.0708;
+    final double kS = 0.039003;
+    final double kV = 0.42996;
+    final double kA = 0.028221;
+    final double kG = 0.10426;
 
     ArmFeedforward feedforward = new ArmFeedforward(kS, kG, kV, kA);
 
 
-    final double kP_FACTOR_ADJUSTMENT = 0.1;
-    final double kP = 5.7517 * kP_FACTOR_ADJUSTMENT;
-    final double kD = 0.29588;
+    final double kP = 0;
+    final double kI = 0;
+    final double kD = 0;
 
-    PIDController pid = new PIDController(kP, 0.1, kD);
+    PIDController pid = new PIDController(kP, kI, kD);
     Shuffleboard.getTab("Lift").add(pid);
 
     return run(() -> {
-      double voltage = feedforward.calculate(positionRadians, 1)
+      double voltage = feedforward.calculate(positionRadians, 0)
         + pid.calculate(lift.getAngleInRadians(), positionRadians);
 
       lift.setVoltage(voltage);
