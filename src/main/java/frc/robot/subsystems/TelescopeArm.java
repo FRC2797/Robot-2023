dddb8143a4d3f1a554df2085a63c195956e5a27d
Joel Singh <joelsingh788@gmail.com>
2023-02-25 10:49:25 -0500

    Change telescope MOTOR_ID to 3

--- src/main/java/frc/robot/subsystems/TelescopeArm.java
+++ src/main/java/frc/robot/subsystems/TelescopeArm.java
@@ -5,7 +5,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TelescopeArm extends SubsystemBase {
-  private final int MOTOR_ID = 4;
+  private final int MOTOR_ID = 3;
   CANSparkMax motor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
 
   public TelescopeArm() {
