fa6c014b0413cca70db4eaae93a4c0968fde34bf
FRC 2797 <team2797@gmail.com>
2023-03-04 15:05:20 -0500

    motor speed is the right trigger axis capped out at 0.15

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,14 +3,26 @@ package frc.robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import static edu.wpi.first.math.MathUtil.interpolate;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Robot extends TimedRobot {
-  private final CANSparkMax grabberMotor = new CANSparkMax(5, MotorType.kBrushless);
+  private final CANSparkMax grabberMotor = new CANSparkMax(2, MotorType.kBrushless);
+  private final double MAX_MOTOR_SPEED = 0.15;
+  XboxController controller = new XboxController(0);
+
+  @Override
+  public void teleopInit() {
+    grabberMotor.setInverted(true);
+  }
 
   @Override
   public void teleopPeriodic() {
-    grabberMotor.set(0.05);
+    double interpolatedMotorSpeed = interpolate(0, MAX_MOTOR_SPEED, controller.getRightTriggerAxis());
+    grabberMotor.set(interpolatedMotorSpeed);
   }
 }
