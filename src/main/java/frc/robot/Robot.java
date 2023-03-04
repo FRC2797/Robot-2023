7f9a105f94644fcf0f1fdc7f304f3c0844c1d680
Joel Singh <joelsingh788@gmail.com>
2023-03-04 12:32:35 -0500

    Testing grabber motor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,8 +1,16 @@
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
+  private final CANSparkMax grabberMotor = new CANSparkMax(5, MotorType.kBrushless);
 
+  @Override
+  public void teleopPeriodic() {
+    grabberMotor.set(0.05);
+  }
 }
