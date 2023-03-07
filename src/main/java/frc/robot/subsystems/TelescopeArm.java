be627d5e155611b948b20eb304682ec44c6232a3
Joel Singh <joelsingh788@gmail.com>
2023-03-07 14:36:01 -0500

    Add set position command to telescope arm

--- src/main/java/frc/robot/subsystems/TelescopeArm.java
+++ src/main/java/frc/robot/subsystems/TelescopeArm.java
@@ -1,19 +1,54 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static java.lang.Math.signum;
+import static java.lang.Math.abs;
 
 public class TelescopeArm extends SubsystemBase {
   private final int MOTOR_ID = 3;
-  CANSparkMax motor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
+  private final CANSparkMax motor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
+  private final RelativeEncoder encoder = motor.getEncoder();
 
   public TelescopeArm() {
     final boolean IS_INVERTED = true;
     motor.setInverted(IS_INVERTED);
+
+    final double UNITS_AT_FULL_EXTENSION = 42.8;
+    encoder.setPositionConversionFactor(1 / UNITS_AT_FULL_EXTENSION);
+    encoder.setPosition(0);
+
+    Shuffleboard.getTab("Telescope Arm").addDouble("Encoder Position", encoder::getPosition);
   }
 
   public void setSpeed(double speed) {
     motor.set(speed);
+  }
+
+  public double getPercentageExtended() {
+    return encoder.getPosition();
+  }
+
+  public CommandBase setPositionCommand(double percentageSetpoint) {
+    final double PROPORTIONAL_TERM = 0.5;
+    final double MIN_TERM = 0.05;
+    final double TOLERANCE = 0.03;
+
+    return run(() -> {
+      double error = percentageSetpoint - getPercentageExtended();
+      double speed = (error * PROPORTIONAL_TERM) + (signum(error) * MIN_TERM);
+      SmartDashboard.putNumber("Error", error);
+      SmartDashboard.putNumber("Speed", speed);
+
+      setSpeed(speed);
+    })
+    .until(() -> abs(getPercentageExtended()) < TOLERANCE)
+    .finallyDo(end -> setSpeed(0));
   }
 }
