1498da3e5c7bf9a20b75d7df1b5493d2db8192c5
Joel Singh <joelsingh788@gmail.com>
2023-03-10 08:10:04 -0500

    Uninvert the front left

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -99,7 +99,7 @@ public class Drivetrain extends SubsystemBase {
 
     MotorControllerGroup leftMotors = new MotorControllerGroup(frontLeft, backLeft);
     MotorControllerGroup rightMotors = new MotorControllerGroup(frontRight, backRight);
-    frontLeft.setInverted(true);
+    frontLeft.setInverted(false);
     rightMotors.setInverted(true);
 
     drive = new DifferentialDrive(leftMotors, rightMotors);
