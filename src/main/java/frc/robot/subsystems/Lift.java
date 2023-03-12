84a20c85f615d8c1e9763c0d92f9bd7fa51fa300
Joel Singh <joelsingh788@gmail.com>
2023-03-12 10:37:05 -0400

    Uninvert lift

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -15,7 +15,7 @@ public class Lift extends SubsystemBase {
 
   public Lift() {
     final int MOTOR_ID = 1;
-    final boolean IS_INVERTED = true;
+    final boolean IS_INVERTED = false;
     final boolean IS_BRAKED = true;
 
     motor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
