a62c56c2b029b573598842d72237fff1f035c88e
Joel Singh <joelsingh788@gmail.com>
2023-03-15 10:32:02 -0400

    Add limelight hasTarget to shuffleboard

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.wpilibj2.command.Commands.*;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -24,11 +25,20 @@ public class Limelight extends SubsystemBase {
     }
   }
 
+  public Limelight() {
+    Shuffleboard.getTab("Driver").addBoolean("Has Target", this::hasTarget);
+  }
+
   private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 
   public double getHorizontalOffset() {
     NetworkTableEntry horizontalOffset = table.getEntry("tx");
     return horizontalOffset.getDouble(ENTRY_NOT_FOUND);
+  }
+
+  public boolean hasTarget() {
+    NetworkTableEntry hasTarget = table.getEntry("tv");
+    return hasTarget.getInteger(0) == 1 ? true : false;
   }
 
   private void switchPipeline(Pipeline pipeline) {
